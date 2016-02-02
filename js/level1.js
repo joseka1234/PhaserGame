5473d625142199ef8ff21e0218be3b633327a61c
joseka1234 <joseka1234@gmail.com>
2016-02-02 15:20:54 +0000

    Cambiando físicas

--- js/level1.js
+++ js/level1.js
@@ -8,10 +8,11 @@ Game.Level1.prototype = {
 		this.x = Game._WIDTH / 2;
 		this.y = Game._HEIGHT / 2;
 
-		this.gravity = 1000;
-		this.speedX = 200;
-		this.speedY = 4;
-		this.drag = 200;
+		this.gravity = 2600;
+		this.speedX = 1500;
+		this.speedY = -1000;
+		this.drag = 800;
+		this.maxSpeed = 500;
 	},
 	create: function () {
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -33,17 +34,24 @@ Game.Level1.prototype = {
 		this.game.physics.arcade.gravity.y = this.gravity;
 		this.player.body.collideWorldBounds = true;
 		this.player.body.drag.setTo(this.drag, 0);
+		this.player.body.maxVelocity.setTo(this.maxSpeed, this.maxSpeed * 10);
 
 		this.game.camera.follow(this.player);
 
 	},
 	update: function () {
 		this.game.physics.arcade.collide(this.player, this.layer);
+
 		if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
-			this.player.body.velocity.x = this.speedX;
-		if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
-			this.player.body.velocity.x = -this.speedX;
-		if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
-			this.player.body.velocity.y = 
+			this.player.body.acceleration.x = this.speedX;
+		else if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+			this.player.body.acceleration.x = -this.speedX;
+		else
+			this.player.body.acceleration.x = 0;
+
+		var onTheGround = this.player.body.touching.down;
+
+		if(onTheGround && this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+			this.player.body.velocity.y = this.speedY;
 	}
 };
